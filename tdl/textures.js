ff88649a985c4429ba24d9291591a431e79f091f
Gregg Tavares <github@greggman.com>
2013-11-12 23:52:58 +0900

    call setParameter instead of texParameter

--- tdl/textures.js
+++ tdl/textures.js
@@ -327,7 +327,7 @@ tdl.textures.Texture2D.prototype.setTexture = function(element) {
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, element);
   if (tdl.textures.isPowerOf2(element.width) &&
       tdl.textures.isPowerOf2(element.height)) {
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    this.setParameter(gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
     gl.generateMipmap(gl.TEXTURE_2D);
   } else {
     this.setParameter(gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
