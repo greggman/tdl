d3db9955445cd03fe0397263466937874a32fe96
Gregg Tavares <github@greggman.com>
2015-01-14 15:18:29 -0800

    make grunt rule to uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,11 +17,43 @@ module.exports = function(grunt) {
     clean: [
         'docs/gen',
     ],
+    uglify: {
+      my_target: {
+        files: {
+          'build/tdl.min.js': [
+            'tdl/base.js',
+            'tdl/buffers.js',
+            'tdl/clock.js',
+            'tdl/fast.js',
+            'tdl/fps.js',
+            'tdl/framebuffers.js',
+            'tdl/fullscreen.js',
+            'tdl/io.js',
+            'tdl/loader.js',
+            'tdl/log.js',
+            'tdl/math.js',
+            'tdl/misc.js',
+            'tdl/models.js',
+            'tdl/particles.js',
+            'tdl/primitives.js',
+            'tdl/programs.js',
+            'tdl/quaternions.js',
+            'tdl/screenshot.js',
+            'tdl/shader.js',
+            'tdl/string.js',
+            'tdl/sync.js',
+            'tdl/textures.js',
+            'tdl/webgl.js',
+          ],
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['clean', 'jsdoc']);
+  grunt.registerTask('default', ['clean', 'jsdoc', 'uglify']);
 };
 
